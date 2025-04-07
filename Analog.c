322b6144a49ff9ebd2016d2f0e787bed6ae05cbb
SANJAY M <118831520+Sanjay-083@users.noreply.github.com>
2025-04-07 14:13:20 +0530

    Update Analog.c

--- Analog.c
+++ Analog.c
@@ -1,8 +1,5 @@
 // Sensors like Potentiometer, LDR, Analog Temperature Sensor (e.g., TMP36)
 
-// ======= CONFIGURATION ========
-#define SENSOR_TYPE 2       // 2 = ANALOG
-#define ANALOG 2
 #define ANALOG_SENSOR_PIN A0  // Connect your analog sensor to this pin
 
 void setup() {
@@ -11,13 +8,11 @@ void setup() {
 }
 
 void loop() {
-  #if SENSOR_TYPE == ANALOG
-    readAnalogSensor();
-  #endif
-  delay(500);
+  readAnalogSensor();
+  delay(500); // Small delay for readability
 }
 
 // ===== ANALOG SENSOR FUNCTION =====
 void readAnalogSensor() {
-  // sensor working logic
+  //logic
 }
